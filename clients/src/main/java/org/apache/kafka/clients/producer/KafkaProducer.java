d2dfd3c34bdea9d8c4795376bd82441d43222620
Ivan Lyutov <ivanlyutov@gmail.com>
2014-08-01 13:47:53 +0300

    bug fixes after rebase

--- clients/src/main/java/org/apache/kafka/clients/producer/KafkaProducer.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/KafkaProducer.java
@@ -189,7 +189,7 @@ public class KafkaProducer<K,V> implements Producer<K,V> {
         List<InetSocketAddress> addresses = ClientUtils.parseAndValidateAddresses(config.getList(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG));
         this.metadata.update(Cluster.bootstrap(addresses), time.milliseconds());
 
-        NetworkClient client = new NetworkClient(new Selector(this.metrics, time , "producer", metricTags),
+        NetworkClient client = new NetworkClient(new Selector(this.metrics, time , "producer", metricTags, config.getBoolean(ProducerConfig.SECURE)),
                                                  this.metadata,
                                                  clientId,
                                                  config.getInt(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION),
