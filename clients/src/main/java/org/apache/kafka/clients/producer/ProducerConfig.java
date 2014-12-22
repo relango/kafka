bb8c0aa7d16c32f287fdfef753ad71488c51a4db
Rajasekar Elango <relango@salesforce.com>
2014-12-22 11:40:06 -0500

    Fix compilation issue rebase with latest commit of 0.8.2

--- clients/src/main/java/org/apache/kafka/clients/producer/ProducerConfig.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/ProducerConfig.java
@@ -188,6 +188,7 @@ public class ProducerConfig extends AbstractConfig {
     public static final String VALUE_SERIALIZER_CLASS_CONFIG = "value.serializer";
     private static final String VALUE_SERIALIZER_CLASS_DOC = "Serializer class for value that implements the <code>Serializer</code> interface.";
 
+    
     static {
         config = new ConfigDef().define(BOOTSTRAP_SERVERS_CONFIG, Type.LIST, Importance.HIGH, BOOSTRAP_SERVERS_DOC)
                                 .define(BUFFER_MEMORY_CONFIG, Type.LONG, 32 * 1024 * 1024L, atLeast(0L), Importance.HIGH, BUFFER_MEMORY_DOC)
